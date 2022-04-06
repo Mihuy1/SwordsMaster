369058523a8779a0745af288cae0a1d3c2810da3
Mihuy1 <s2000954@edu.bc.fi>
2022-04-06 18:26:57 +0300

    Siivottu EnemyHealthManager

--- Assets/Scripts/EnemyHealthManager.cs
+++ Assets/Scripts/EnemyHealthManager.cs
@@ -10,48 +10,38 @@ public class EnemyHealthManager : MonoBehaviour
     public int maxHealth = 100;
     public int currenthealth;
 
-    private Transform target;
-    public NavMeshAgent agent;
-
-    public float speed = 5f;
-    public float stoppingDistance;
-    public float range;
-
-    private float timeBtwAttack;
-    public float startTimeBtwAttack;
-
     public ThirdPersonMovementScript _player;
     public HealthBar healthbar;
 
-    private void Awake()
-    {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        timeBtwAttack = startTimeBtwAttack;
-    }
+    public NavMeshAgent agent;
+
+    private Transform target;
+
+    public float range;
+    public float startTimeBtwAttack;
+    public float stoppingDistance;
+
+    private float timeBtwAttack;
 
     // Start is called before the first frame update
     public void Start()
     {
 
         currenthealth = maxHealth;
-
         healthbar.SetMaxHealth(maxHealth);
+
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        timeBtwAttack = startTimeBtwAttack;
     }
 
     public void Update()
     {
-        agent.SetDestination(target.position);
+        if (Vector3.Distance(transform.position, target.position) <= range)
+        {
+            agent.SetDestination(target.position);
+        }
 
-        //Vector3 dir = target.position - transform.position;
-        //Quaternion rotation = Quaternion.LookRotation(dir);
-        //transform.rotation = rotation;
-
-            //if (Vector3.Distance(transform.position, target.position) > stoppingDistance)
-            //{
-            //    transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-            //}
-
-         if (Vector3.Distance(transform.position, target.position) < stoppingDistance)
+        if (Vector3.Distance(transform.position, target.position) < stoppingDistance)
         {
             transform.position = this.transform.position;
             Attack();
@@ -61,9 +51,27 @@ public class EnemyHealthManager : MonoBehaviour
         {
             if (Vector3.Distance(transform.position, target.position) < range)
             {
-                TakeDamage(15);
+               TakeDamage(15);
             }
         }
+    }
+
+        public void TakeDamage(int damage)
+    {
+        currenthealth -= damage;
+        healthbar.SetHealth(currenthealth);
+
+        if (currenthealth <= 0)
+        {
+            Die();
+        }
+
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+        Debug.Log("+10 XP!");
     }
 
     public void Attack()
@@ -75,27 +83,10 @@ public class EnemyHealthManager : MonoBehaviour
             Debug.Log("Attacking");
 
             timeBtwAttack = startTimeBtwAttack;
-        } else
+        }
+        else
         {
             timeBtwAttack -= Time.deltaTime;
         }
-    }
-
-    public void TakeDamage(int damage)
-    {
-        currenthealth -= damage;
-        healthbar.SetHealth(currenthealth);
-
-        if (currenthealth <= 0)
-        {
-            Die();
-        }
-
-}
-
-    public void Die()
-    {
-        Destroy(gameObject);
-        Debug.Log("+10 XP!");
     }
 }
