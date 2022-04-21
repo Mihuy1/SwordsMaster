be7d1c1776b8248757f8ee1110e053fa1c5528d4
Mihuy1 <s2000954@edu.bc.fi>
2022-04-21 10:39:35 +0300

    Lisätty WaveSpawner.cs
    Ensimmäinen osa spawner systeemiä tehty.

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public enum SpawnState {spawning, waiting, counting }
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountDown;
+
+    private SpawnState state = SpawnState.counting;
+
+
+     void Start()
+    {
+        waveCountDown = timeBetweenWaves;
+    }
+
+     void Update()
+    { 
+        if (waveCountDown <= 0)
+        {
+            if (state != SpawnState.spawning)
+            {
+                // Start spawning wave
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        } 
+        else
+        {
+            waveCountDown -= Time.deltaTime;
+        }
+    }
+
+    IEnumerator SpawnWave (Wave _wave)
+    {
+        state = SpawnState.spawning;
+
+        for(int i = 0; i < _wave.count;i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+        state = SpawnState.waiting;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        // Spawn enemy
+        Debug.Log("Spawning enemy " + _enemy.name);
+    }
+}
